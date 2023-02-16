884dc2c929b85bee63fe6bdc9cc2f3531b259dca
alripol <125578558+alripol@users.noreply.github.com>
2023-02-16 15:49:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '5824682629:AAEESg_yUlWXKIhHZ8hb0FGv9kBSr7rsLyI';
 
+const token = '5824682629:AAEESg_yUlWXKIhHZ8hb0FGv9kBSr7rsLyI';
 const sourceChatId = -1001658917150; 
 const targetChatId = -1001819375133; 
 
